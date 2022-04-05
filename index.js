e45c2516be42d6b08d2c4e9b008a6f9da47d8ff8
bgm5768 <bgm5768@gmail.com>
2022-04-05 10:02:43 +0900

    change origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://112.171.31.123:3002" , "https://nonamed.vercel.app/"],
+    origin: ["http://112.171.31.123:3002" , "https://nonamed.vercel.app"],
     // or with an array of origins
     // origin: ["https://my-frontend.com", "https://my-other-frontend.com", "http://localhost:3000"],
     credentials: true
